2c0174cd2e43aba20775f90f088056ff2f2745ba
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-07 13:22:06 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -47,8 +47,8 @@ function App() {
 
   return (
     <>
-      
-      <img src="https://taplink.st/a/0/c/0/8/4d0981.jpg?4"></img>
+
+      <img src="https://taplink.st/a/0/c/0/8/4d0981.jpg?4" alt="logo"></img>
       <Cart cartItems={cartItems} onCheckout={onCheckout}/>
       <div className="cards__container">
         {foods.map((food) => {
