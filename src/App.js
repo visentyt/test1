df7123829af1df98b770c2391b1fe1d74f0c245d
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-10 11:09:19 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -47,39 +47,6 @@ function App() {
 
   return (
       <>
-          <div className="menu">
-              <button onClick="openContent('content1')">Коктейли</button>
-              <button onClick="openContent('content2')">Пиво</button>
-              <button onClick="openContent('content3')">Вино</button>
-              <button onClick="openContent('content4')">Игристое вино</button>
-              <button onClick="openContent('content5')">Крепкий алкоголь</button>
-              <button onClick="openContent('content6')">Шоты</button>
-              <button onClick="openContent('content7')">Безалкогольные напитки</button>
-          </div>
-
-          <div id="content1" className="content" style="display: none;">
-              Коктейли
-          </div>
-          <div id="content2" className="content" style="display: none;">
-              Пиво
-          </div>
-          <div id="content3" className="content" style="display: none;">
-              Вино
-          </div>
-          <div id="content4" className="content" style="display: none;">
-              Игристое вино
-          </div>
-          <div id="content5" className="content" style="display: none;">
-              Крепкий алкоголь
-          </div>
-
-          <div id="content6" class="content" style="display: none;">
-              Шоты
-          </div>
-
-          <div id="content7" class="content" style="display: none;">
-              Безалкогольные напитки
-          </div>
         <div className="circle-img-container">
           <img className="circle-img" src="https://taplink.st/a/0/c/0/8/4d0981.jpg?4" alt="logo" />
         </div>
@@ -91,13 +58,6 @@ function App() {
             );
           })}
         </div>
-              function openContent(contentId) {
-              var contents = document.getElementsByClassName("content");
-              for (var i = 0; i < contents.length; i++) {
-              contents[i].style.display = "none";
-          }
-              document.getElementById(contentId).style.display = "block";
-          }
       </>
   );
 }
