7195b85d8a1c38ddfdeca6765cda3ea71e3b295a
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-10 11:20:47 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,64 +2,75 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./Components/Card/Card";
 import Cart from "./Components/Cart/Cart";
+import Menu from "./Components/Menu/Menu";
 const { getData } = require("./db/db");
 const foods = getData();
 
 const tele = window.Telegram.WebApp;
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
+    const [activeCategory, setActiveCategory] = useState(null);
 
-  useEffect(() => {
-    tele.ready();
-  });
+    useEffect(() => {
+        tele.ready();
+    });
 
-  const onAdd = (food) => {
-    const exist = cartItems.find((x) => x.id === food.id);
-    if (exist) {
-      setCartItems(
-          cartItems.map((x) =>
-              x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
-          )
-      );
-    } else {
-      setCartItems([...cartItems, { ...food, quantity: 1 }]);
-    }
-  };
-
-  const onRemove = (food) => {
-    const exist = cartItems.find((x) => x.id === food.id);
-    if (exist.quantity === 1) {
-      setCartItems(cartItems.filter((x) => x.id !== food.id));
-    } else {
-      setCartItems(
-          cartItems.map((x) =>
-              x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
-          )
-      );
-    }
-  };
-
-  const onCheckout = () => {
-    tele.MainButton.text = "Оплатить";
-    tele.MainButton.show();
-  };
-
-  return (
-      <>
-        <div className="circle-img-container">
-          <img className="circle-img" src="https://taplink.st/a/0/c/0/8/4d0981.jpg?4" alt="logo" />
-        </div>
-        <Cart cartItems={cartItems} onCheckout={onCheckout}/>
-        <div className="cards__container">
-          {foods.map((food) => {
-            return (
-                <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+    const onAdd = (food) => {
+        const exist = cartItems.find((x) => x.id === food.id);
+        if (exist) {
+            setCartItems(
+                cartItems.map((x) =>
+                    x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
+                )
             );
-          })}
-        </div>
-      </>
-  );
+        } else {
+            setCartItems([...cartItems, { ...food, quantity: 1 }]);
+        }
+    };
+
+    const onRemove = (food) => {
+        const exist = cartItems.find((x) => x.id === food.id);
+        if (exist.quantity === 1) {
+            setCartItems(cartItems.filter((x) => x.id !== food.id));
+        } else {
+            setCartItems(
+                cartItems.map((x) =>
+                    x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+                )
+            );
+        }
+    };
+
+    const onCheckout = () => {
+        tele.MainButton.text = "Оплатить";
+        tele.MainButton.show();
+    };
+
+    const filterFoodsByCategory = (category) => {
+        if (category === null) {
+            return foods;
+        } else {
+            return foods.filter((food) => food.category === category);
+        }
+    };
+
+    return (
+        <>
+            <div className="circle-img-container">
+                <img className="circle-img" src="https://taplink.st/a/0/c/0/8/4d0981.jpg?4" alt="logo" />
+            </div>
+            <Cart cartItems={cartItems} onCheckout={onCheckout}/>
+            <Menu setActiveCategory={setActiveCategory} />
+            <div className="cards__container">
+                {filterFoodsByCategory(activeCategory).map((food) => {
+                    return (
+                        <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+                    );
+                })}
+            </div>
+        </>
+    );
 }
 
 export default App;
