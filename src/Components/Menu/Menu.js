91ee87ba035f1acab2a3db4b6924360bf49b81b9
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-10 16:13:58 +1000

    Add files via upload

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,10 +1,12 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import "./Menu.css";
 
 function Menu({ setActiveCategory, onSearch }) {
     const menuRef = useRef(null); // Ссылка на DOM-элемент меню
+    const [activeCategory, setActiveCategory] = useState(null); // Активная категория
 
     const handleCategoryClick = (category) => {
+        setActiveCategory(category);
         setActiveCategory(category);
     };
 
@@ -13,42 +15,59 @@ function Menu({ setActiveCategory, onSearch }) {
         onSearch(keyword);
     };
 
-    // Функция пролистывания пальцем вправо или влево
-    const handleTouchMove = (event) => {
+    const scrollRight = () => {
         if (menuRef.current) {
-            const touch = event.touches[0];
-            const touchX = touch.clientX;
-            const menuRect = menuRef.current.getBoundingClientRect();
-            const menuCenterX = menuRect.left + menuRect.width / 2;
+            menuRef.current.scrollLeft += 100;
+        }
+    };
 
-            if (touchX > menuCenterX) {
-                // Пролистывание вправо
-                menuRef.current.scrollLeft += 10;
-            } else {
-                // Пролистывание влево
-                menuRef.current.scrollLeft -= 10;
-            }
+    const scrollLeft = () => {
+        if (menuRef.current) {
+            menuRef.current.scrollLeft -= 100;
         }
     };
 
     return (
         <div className="menu-wrapper">
-            <div className="menu" ref={menuRef} onTouchMove={handleTouchMove}>
-                <button onClick={() => handleCategoryClick(null)}>Все</button>
-                <button onClick={() => handleCategoryClick("hookah")}>Кальян</button>
-                <button onClick={() => handleCategoryClick("beer")}>Пиво</button>
-                <button onClick={() => handleCategoryClick("shot")}>Шоты</button>
-                <button onClick={() => handleCategoryClick("beer")}>Пиво</button>
-                <button onClick={() => handleCategoryClick("shot")}>Шоты</button>
-                <button onClick={() => handleCategoryClick("beer")}>Пиво</button>
-                <button onClick={() => handleCategoryClick("shot")}>Шоты</button>
-                <button onClick={() => handleCategoryClick("beer")}>Пиво</button>
-                <button onClick={() => handleCategoryClick("shot")}>Шоты</button>              <button onClick={() => handleCategoryClick("beer")}>Пиво</button>
-                <button onClick={() => handleCategoryClick("shot")}>Шоты</button>
-
+            {/* Фиксированное горизонтальное меню */}
+            <div className="menu" ref={menuRef}>
+                <button
+                    onClick={() => handleCategoryClick(null)}
+                    className={activeCategory === null ? "active" : ""}
+                >
+                    Все
+                </button>
+                <button
+                    onClick={() => handleCategoryClick("hookah")}
+                    className={activeCategory === "hookah" ? "active" : ""}
+                >
+                    Кальян
+                </button>
+                <button
+                    onClick={() => handleCategoryClick("beer")}
+                    className={activeCategory === "beer" ? "active" : ""}
+                >
+                    Пиво
+                </button>
+                <button
+                    onClick={() => handleCategoryClick("shot")}
+                    className={activeCategory === "shot" ? "active" : ""}
+                >
+                    Шоты
+                </button>
                 {/* Добавьте другие кнопки для остальных категорий */}
             </div>
-            {/* Удалите кнопки menu-navigation */}
+
+            {/* Кнопки пролистывания меню */}
+            {menuRef.current &&
+                menuRef.current.scrollWidth > menuRef.current.clientWidth && (
+                    <div className="menu-navigation">
+                        <button onClick={scrollLeft}>&lt;</button>
+                        <button onClick={scrollRight}>&gt;</button>
+                    </div>
+                )}
+
+            {/* Фильтрация карточек по активной категории */}
             <input
                 type="text"
                 placeholder="Поиск..."
