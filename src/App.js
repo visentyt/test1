d3e41b9da7a5f2285a9641bab0a908af32ab36be
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-10 11:05:57 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -91,6 +91,15 @@ function App() {
             );
           })}
         </div>
+          <script>
+              function openContent(contentId) {
+              var contents = document.getElementsByClassName("content");
+              for (var i = 0; i < contents.length; i++) {
+              contents[i].style.display = "none";
+          }
+              document.getElementById(contentId).style.display = "block";
+          }
+          </script>
       </>
   );
 }
