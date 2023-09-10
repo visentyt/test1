b14002dfa56be314cf580b3a507103945bf81ce7
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-10 11:34:53 +1000

    Add files via upload

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -16,18 +16,22 @@ function Menu() {
         }
     };
 
-    function Menu({ setActiveCategory }) {
-        const handleCategoryClick = (category) => {
-            setActiveCategory(category);
-        };
-
-        return (
-            <div className="menu">
-                <button onClick={() => handleCategoryClick("hookah")}>Кальян</button>
-                <button onClick={() => handleCategoryClick("beer")}>Пиво</button>
-                <button onClick={() => handleCategoryClick("shot")}>Шоты</button>
+    return (
+        <div className="menu">
+            <div className="menu__buttons">
+                <button onClick={() => setCategory("hookah")}>Кальян</button>
+                <button onClick={() => setCategory("beer")}>Пиво</button>
+                <button onClick={() => setCategory("shot")}>Шоты</button>
+                {/* Добавьте другие кнопки для остальных категорий */}
             </div>
-        );
-    }
+
+            <div className="menu__cards">
+                {filterCards(category).map((item) => (
+                    <Card key={item.id} food={item} />
+                ))}
+            </div>
+        </div>
+    );
+}
 
 export default Menu;
