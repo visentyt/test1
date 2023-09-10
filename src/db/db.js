f2f157b3636670c686d10b704e36e9873d38e715
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-10 16:57:32 +1000

    Add files via upload

--- src/db/db.js
+++ src/db/db.js
@@ -3,11 +3,17 @@ import pivo1Img from "../images/pivo1.png";
 import cocaImg from "../images/coca.png";
 import shot1Img from "../images/shot1.png";
 
-export function getData() {
-  return [
-    { title: "Кальян", price: 1000, Image: hooka1Img, id: 1, category: "hookah" },
-    { title: "Пиво", price: 200, Image: pivo1Img, id: 2, category: "beer" },
-    { title: "Шот", price: 300, Image: shot1Img, id: 3, category: "shot" },
-    { title: "Кола", price: 139, Image: cocaImg, id: 4, category: "drink" },
-  ];
+export let products = [
+  { title: "Кальян", price: 1000, Image: hooka1Img, id: 1, category: "hookah" },
+  { title: "Пиво", price: 200, Image: pivo1Img, id: 2, category: "beer" },
+  { title: "Шот", price: 300, Image: shot1Img, id: 3, category: "shot" },
+  { title: "Кола", price: 139, Image: cocaImg, id: 4, category: "drink" },
+];
+
+export function addProduct(product) {
+  products.push(product);
+}
+
+export function deleteProduct(productId) {
+  products = products.filter((product) => product.id !== productId);
 }
