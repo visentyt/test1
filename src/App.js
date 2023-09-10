5e555a4f27ea7bb75efdf1640612e08992c23187
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-10 11:46:56 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 import Card from "./Components/Card/Card";
 import Cart from "./Components/Cart/Cart";
 import Menu from "./Components/Menu/Menu";
-const { getData } = require("./db/db");
-const foods = getData();
-
-const tele = window.Telegram.WebApp;
+import { getData } from "./db/db";
 
 function App() {
     const [cartItems, setCartItems] = useState([]);
     const [activeCategory, setActiveCategory] = useState(null);
+    const [searchKeyword, setSearchKeyword] = useState("");
+
+    const foods = getData();
 
     const onAdd = (food) => {
         const exist = cartItems.find((x) => x.id === food.id);
@@ -39,8 +39,8 @@ function App() {
     };
 
     const onCheckout = () => {
-        tele.MainButton.text = "Оплатить";
-        tele.MainButton.show();
+        window.Telegram.MainButton.text = "Оплатить";
+        window.Telegram.MainButton.show();
     };
 
     const filterFoodsByCategory = (category) => {
@@ -51,19 +51,36 @@ function App() {
         }
     };
 
+    const searchFoods = (keyword) => {
+        setSearchKeyword(keyword);
+    };
+
+    const filteredFoods = searchKeyword
+        ? foods.filter((food) =>
+            food.title.toLowerCase().includes(searchKeyword.toLowerCase())
+        )
+        : filterFoodsByCategory(activeCategory);
+
     return (
         <>
             <div className="circle-img-container">
-                <img className="circle-img" src="https://taplink.st/a/0/c/0/8/4d0981.jpg?4" alt="logo" />
+                <img
+                    className="circle-img"
+                    src="https://taplink.st/a/0/c/0/8/4d0981.jpg?4"
+                    alt="logo"
+                />
             </div>
-            <Cart cartItems={cartItems} onCheckout={onCheckout}/>
-            <Menu setActiveCategory={setActiveCategory} />
+            <Cart cartItems={cartItems} onCheckout={onCheckout} />
+            <Menu setActiveCategory={setActiveCategory} onSearch={searchFoods} />
             <div className="cards__container">
-                {filterFoodsByCategory(activeCategory).map((food) => {
-                    return (
-                        <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
-                    );
-                })}
+                {filteredFoods.map((food) => (
+                    <Card
+                        key={food.id}
+                        food={food}
+                        onAdd={onAdd}
+                        onRemove={onRemove}
+                    />
+                ))}
             </div>
         </>
     );
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./Menu.css";
 
-function Menu({ setActiveCategory }) {
+function Menu({ setActiveCategory, onSearch }) {
     const handleCategoryClick = (category) => {
         setActiveCategory(category);
     };
 
+    const handleSearch = (event) => {
+        const keyword = event.target.value;
+        onSearch(keyword);
+    };
+
     return (
         <div className="menu">
+            <button onClick={() => handleCategoryClick(null)}>Все</button>
             <button onClick={() => handleCategoryClick("hookah")}>Кальян</button>
             <button onClick={() => handleCategoryClick("beer")}>Пиво</button>
             <button onClick={() => handleCategoryClick("shot")}>Шоты</button>
             {/* Добавьте другие кнопки для остальных категорий */}
+            <input
+                type="text"
+                placeholder="Поиск..."
+                onChange={handleSearch}
+            />
         </div>
     );
 }
