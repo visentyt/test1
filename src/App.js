b461e143db29992ce539c9faa49ed41c40ed8d1b
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-14 02:37:41 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -105,43 +105,45 @@ function App() {
                     </div>
                 </div>
             )}
-            <Cart cartItems={cartItems} onCheckout={onCheckout} />
-            <div className="burger-menu" onClick={toggleMenu}>
-                <div className="burger-line"></div>
-                <div className="burger-line"></div>
-                <div className="burger-line"></div>
-            </div>
-            <div id="menu">
-                <div className="menu-item" onClick={() => showCards(null)}>
-                    Все
-                </div>
-                <div className="menu-item" onClick={() => showCards("hookah")}>
-                    Кальян
-                </div>
-                <div className="menu-item" onClick={() => showCards("beer")}>
-                    Пиво
-                </div>
-                <div className="menu-item" onClick={() => showCards("shot")}>
-                    Шоты
-                </div>
-                <div className="menu-item" onClick={() => showCards("drink")}>
-                    Напитки
-                </div>
-                <div className="menu-item" onClick={() => showCards("eat")}>
-                    Закуски
-                </div>
-                <div className="menu-item" onClick={() => showCards("kokteil")}>
-                    Коктейли
-                </div>
-            </div>
-            <div className="cards__container">
-                {filteredFoods.map((food) => (
-                    <Card key={food.id} food={food} onAdd={onAdd} onRemove={onRemove} />
-                ))}
-            </div>
+
 
             {showAbout && <div className="about-section">Лол</div>}
-            {showMenu && <div className="menu-section">Попа</div>}
+            {showMenu && <div className="menu-section">
+                <Cart cartItems={cartItems} onCheckout={onCheckout} />
+                <div className="burger-menu" onClick={toggleMenu}>
+                    <div className="burger-line"></div>
+                    <div className="burger-line"></div>
+                    <div className="burger-line"></div>
+                </div>
+                <div id="menu">
+                    <div className="menu-item" onClick={() => showCards(null)}>
+                        Все
+                    </div>
+                    <div className="menu-item" onClick={() => showCards("hookah")}>
+                        Кальян
+                    </div>
+                    <div className="menu-item" onClick={() => showCards("beer")}>
+                        Пиво
+                    </div>
+                    <div className="menu-item" onClick={() => showCards("shot")}>
+                        Шоты
+                    </div>
+                    <div className="menu-item" onClick={() => showCards("drink")}>
+                        Напитки
+                    </div>
+                    <div className="menu-item" onClick={() => showCards("eat")}>
+                        Закуски
+                    </div>
+                    <div className="menu-item" onClick={() => showCards("kokteil")}>
+                        Коктейли
+                    </div>
+                </div>
+                <div className="cards__container">
+                    {filteredFoods.map((food) => (
+                        <Card key={food.id} food={food} onAdd={onAdd} onRemove={onRemove} />
+                    ))}
+                </div>
+            </div>}
         </>
     );
 }
