90110c33956e2f89507f13a25aa4cc3a50ccd4ec
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-14 15:07:57 +1000

    Add files via upload

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,48 +2,25 @@ import React, { useState } from "react";
 import "./Card.css";
 import Button from "../Button/Button";
 
-const tele = window.Telegram.WebApp;
-
-function Card({ food, onAdd, onRemove, cartItems }) {
+function Card({ food, onAdd, onRemove, onCheckout }) {
     const [count, setCount] = useState(0);
     const { title, Image, price } = food;
 
     const handleIncrement = () => {
         setCount(count + 1);
         onAdd(food);
-        updateCheckoutButton();
+        onCheckout(); // вызываем функцию onCheckout
     };
 
     const handleDecrement = () => {
         setCount(count - 1);
         onRemove(food);
-        updateCheckoutButton();
-    };
-
-    const updateCheckoutButton = () => {
-        const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0); // Обновляем totalPrice с учетом текущего состояния корзины
-        const totalPriceText = `Цена: ${totalPrice.toFixed(2)}₽`;
-        tele.MainButton.show();
-        tele.MainButton.setParams({
-            text: totalPriceText,
-            show: true,
-            textColor: "#ffffff",
-            color: "#A9A9A9"
-        });
+        onCheckout(); // вызываем функцию onCheckout
     };
 
     return (
         <div className="card">
-      <span className={count !== 0 ? "card__badge" : "card__badge--hidden"}>
-        {count}
-      </span>
-            <div className="image__container">
-                <img src={Image} alt={title} />
-            </div>
-            <h4 className="card__title">
-                {title} <span className="card__price">{price}₽</span>
-            </h4>
-
+            {/* остальной код компонента */}
             <div className="btn-container">
                 <Button title={"+"} type={"add"} onClick={handleIncrement} />
                 {count !== 0 ? (
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,33 +1,20 @@
 import React from "react";
 import "./Cart.css";
 import Button from "../Button/Button";
-import Card from "../Card/Card";
+function Cart({ cartItems, onCheckout }) {
+  const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
 
-function Cart({ cartItems, onCheckout, onAdd, onRemove }) {
-    const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
-
-    return (
-        <div className="cart__container">
-            {cartItems.length === 0 ? "Вы ничего не добавили в корзину" : ""}
-            <br />
-            <span className="">Цена: {totalPrice.toFixed(2)}₽</span>
-            <Button
-                title={cartItems.length === 0 ? "Заказать !" : "Оплатить"}
-                type={"checkout"}
-                disable={cartItems.length === 0 ? true : false}
-                onClick={onCheckout}
-            />
-            {cartItems.map((food) => (
-                <Card
-                    key={food.id}
-                    food={food}
-                    onAdd={onAdd} // Передаем функцию onAdd
-                    onRemove={onRemove} // Передаем функцию onRemove
-                    cartItems={cartItems}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="cart__container">
+      {cartItems.length === 0 ? "Вы ничего не добавили в корзину" : ""}
+      <br /> <span className="">Цена: {totalPrice.toFixed(2)}₽</span>
+      <Button
+        title={`${cartItems.length === 0 ? "Заказать !" : "Оплатить"} `}
+        type={"checkout"}
+        disable={cartItems.length === 0 ? true : false}
+        onClick={onCheckout}
+      />
+    </div>
+  );
 }
-
 export default Cart;
