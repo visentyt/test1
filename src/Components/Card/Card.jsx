9333cf7c99bf788f0586b0d2fe23ed7eb2d07da0
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-14 17:09:06 +1000

    Add files via upload

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,9 +4,10 @@ import Button from "../Button/Button";
 
 const tele = window.Telegram.WebApp;
 
-function Card({ food, onAdd, onRemove, totalPrice }) {
+function Card({ food, onAdd, onRemove }) {
     const [count, setCount] = useState(0);
     const { title, Image, price } = food;
+    const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
 
     const handleIncrement = () => {
         setCount(count + 1);
@@ -28,10 +29,21 @@ function Card({ food, onAdd, onRemove, totalPrice }) {
     };
 
     return (
+        <>
+        <div className="cart__container">
+            {cartItems.length === 0 ? "Вы ничего не добавили в корзину" : ""}
+            <br /> <span className="">Цена: {totalPrice.toFixed(2)}₽</span>
+            <Button
+                title={`${cartItems.length === 0 ? "Заказать !" : "Оплатить"} `}
+                type={"checkout"}
+                disable={cartItems.length === 0 ? true : false}
+                onClick={onCheckout}
+            />
+        </div>
         <div className="card">
-      <span className={count !== 0 ? "card__badge" : "card__badge--hidden"}>
-        {count}
-      </span>
+            <span className={count !== 0 ? "card__badge" : "card__badge--hidden"}>
+                {count}
+            </span>
             <div className="image__container">
                 <img src={Image} alt={title} />
             </div>
@@ -46,6 +58,7 @@ function Card({ food, onAdd, onRemove, totalPrice }) {
                 )}
             </div>
         </div>
+        </>
     );
 }
 
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import "./Cart.css";
 import Button from "../Button/Button";
-
 function Cart({ cartItems, onCheckout }) {
-    const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
+  const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
 
-    const handleCheckout = () => {
-        if (onCheckout) {
-            onCheckout(totalPrice);
-        }
-    };
-
-    return (
-        <div className="cart__container">
-            {cartItems.length === 0 ? "Вы ничего не добавили в корзину" : ""}
-            <br /> <span className="">Цена: {totalPrice?.toFixed(2)}₽</span>
-            <Button
-                title={cartItems.length === 0 ? "Заказать !" : "Оплатить"}
-                type={"checkout"}
-                disable={cartItems.length === 0 ? true : false}
-                onClick={handleCheckout}
-            />
-        </div>
-    );
+  return (
+    <div className="cart__container">
+      {cartItems.length === 0 ? "Вы ничего не добавили в корзину" : ""}
+      <br /> <span className="">Цена: {totalPrice.toFixed(2)}₽</span>
+      <Button
+        title={`${cartItems.length === 0 ? "Заказать !" : "Оплатить"} `}
+        type={"checkout"}
+        disable={cartItems.length === 0 ? true : false}
+        onClick={onCheckout}
+      />
+    </div>
+  );
 }
-
 export default Cart;
