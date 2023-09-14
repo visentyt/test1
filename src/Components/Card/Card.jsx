5d1e0d9451fca14b78208685eb48d32421078c49
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-14 17:15:28 +1000

    Add files via upload

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,7 +4,7 @@ import Button from "../Button/Button";
 
 const tele = window.Telegram.WebApp;
 
-function Card({ food, onAdd, onRemove, cartItems, }) {
+function Card({ food, onAdd, onRemove, cartItems }) {
     const [count, setCount] = useState(0);
     const { title, Image, price } = food;
     const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
@@ -30,34 +30,39 @@ function Card({ food, onAdd, onRemove, cartItems, }) {
 
     return (
         <>
-        <div className="cart__container">
-            {cartItems.length === 0 ? "Вы ничего не добавили в корзину" : ""}
-            <br /> <span className="">Цена: {totalPrice.toFixed(2)}₽</span>
-            <Button
-                title={`${cartItems.length === 0 ? "Заказать !" : "Оплатить"} `}
-                type={"checkout"}
-                disable={cartItems.length === 0 ? true : false}
-                onClick={onCheckout}
-            />
-        </div>
-        <div className="card">
-            <span className={count !== 0 ? "card__badge" : "card__badge--hidden"}>
-                {count}
-            </span>
-            <div className="image__container">
-                <img src={Image} alt={title} />
-            </div>
-            <h4 className="card__title">
-                {title} <span className="card__price">{price}₽</span>
-            </h4>
-
-            <div className="btn-container">
-                <Button title={"+"} type={"add"} onClick={handleIncrement} />
-                {count !== 0 && (
-                    <Button title={"-"} type={"remove"} onClick={handleDecrement} />
+            <div className="cart__container">
+                {cartItems.length === 0 && "Вы ничего не добавили в корзину"}
+                {cartItems.length !== 0 && (
+                    <>
+                        <br />
+                        <span className="">Цена: {totalPrice.toFixed(2)}₽</span>
+                        <Button
+                            title={cartItems.length === 0 ? "Заказать!" : "Оплатить"}
+                            type={"checkout"}
+                            disable={cartItems.length === 0 ? true : false}
+                            onClick={onCheckout}
+                        />
+                    </>
                 )}
             </div>
-        </div>
+            <div className="card">
+        <span className={count !== 0 ? "card__badge" : "card__badge--hidden"}>
+          {count}
+        </span>
+                <div className="image__container">
+                    <img src={Image} alt={title} />
+                </div>
+                <h4 className="card__title">
+                    {title} <span className="card__price">{price}₽</span>
+                </h4>
+
+                <div className="btn-container">
+                    <Button title={"+"} type={"add"} onClick={handleIncrement} />
+                    {count !== 0 && (
+                        <Button title={"-"} type={"remove"} onClick={handleDecrement} />
+                    )}
+                </div>
+            </div>
         </>
     );
 }
