f5de7c6826b401e461a6ac9783349a19d0ff513f
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-14 15:15:23 +1000

    Add files via upload

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,33 +2,32 @@ import React, { useState } from "react";
 import "./Card.css";
 import Button from "../Button/Button";
 
-const tele = window.Telegram.WebApp;
-
 function Card({ food, onAdd, onRemove }) {
     const [count, setCount] = useState(0);
+    const { title, Image, price } = food;
 
     const handleIncrement = () => {
         setCount(count + 1);
         onAdd(food);
-        onCheckout();
     };
 
     const handleDecrement = () => {
         setCount(count - 1);
         onRemove(food);
-        onCheckout();
-    };
-
-    const onCheckout = () => {
-        tele.MainButton.text = "Оплатить";
-        tele.MainButton.show();
-        tele.MainButton.textColor = "#ffffff";
-        tele.MainButton.color = "#A9A9A9"; //изменяем цвет бэкграунда кнопки
     };
 
     return (
         <div className="card">
-            {/* остальной код компонента */}
+      <span className={${count !== 0 ? "card__badge" : "card__badge--hidden"}}>
+        {count}
+      </span>
+            <div className="image__container">
+                <img src={Image} alt={title} />
+            </div>
+            <h4 className="card__title">
+                {title} <span className="card__price">{price}₽</span>
+            </h4>
+
             <div className="btn-container">
                 <Button title={"+"} type={"add"} onClick={handleIncrement} />
                 {count !== 0 ? (
