d035017738ea6e7a1614ee7f73a990b199f02bde
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-14 13:42:23 +1000

    Add files via upload

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Card.css";
 import Button from "../Button/Button";
 
-function Card({ food, onAdd, onRemove }) {
+function Card({ food, onAdd, onRemove, totalPrice }) {
     const [count, setCount] = useState(0);
     const { title, Image, price } = food;
+
+    useEffect(() => {
+        setCount(getItemCount()); // Устанавливаем начальное значение счетчика при загрузке компонента
+    }, []);
+
+    const getItemCount = () => {
+        const item = totalPrice.find((item) => item.id === food.id);
+        return item ? item.quantity : 0;
+    };
 
     const handleIncrement = () => {
         setCount(count + 1);
@@ -16,13 +25,15 @@ function Card({ food, onAdd, onRemove }) {
         onRemove(food);
     };
 
-
+    useEffect(() => {
+        setCount(getItemCount()); // Обновляем значение счетчика при изменении списка totalPrice
+    }, [totalPrice]);
 
     return (
         <div className="card">
-      <span className={`${count !== 0 ? "card__badge" : "card__badge--hidden"}`}>
-        {count}
-      </span>
+            <span className={`${count !== 0 ? "card__badge" : "card__badge--hidden"}`}>
+                {count}
+            </span>
             <div className="image__container">
                 <img src={Image} alt={title} />
             </div>
@@ -32,10 +43,8 @@ function Card({ food, onAdd, onRemove }) {
 
             <div className="btn-container">
                 <Button title={"+"} type={"add"} onClick={handleIncrement} />
-                {count !== 0 ? (
+                {count !== 0 && (
                     <Button title={"-"} type={"remove"} onClick={handleDecrement} />
-                ) : (
-                    ""
                 )}
             </div>
         </div>
