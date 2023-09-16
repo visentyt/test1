3dc0e0606a42d4d7879886c63ec68b13d7de4b93
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-16 20:09:10 +1000

    Add files via upload

--- src/Components/BottomNavigation/BottomNavigation.js
+++ src/Components/BottomNavigation/BottomNavigation.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import AboutUs from "./AboutUs";
 import Menu from "./Menu";
 import Complaints from "./Complaints";
