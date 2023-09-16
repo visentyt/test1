f2a6ac1908879ed7694f6b59e1102453fafff781
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-16 16:21:24 +1000

    Add files via upload

--- src/Components/BottomNavigation/AboutUs.js
+++ src/Components/BottomNavigation/AboutUs.js
@@ -1,7 +1,113 @@
-import React from "react";
+import React, { useEffect } from "react"; // Импортируем useEffect из React
+import "./test.css";
 
 function AboutUs() {
-    return <div>About Us</div>;
+    useEffect(() => {
+        // Используем useEffect для установки обработчиков событий после монтирования компонента
+        const button = document.querySelector(".menu__button");
+        const menu = document.querySelector(".menu__body");
+        const close = document.querySelector(".menu__header button");
+        const overlay = document.querySelector(".menu__overlay");
+
+        function showMenu() {
+            button.setAttribute("hidden", "");
+            menu.removeAttribute("hidden");
+            overlay.removeAttribute("hidden");
+        }
+
+        function hideMenu() {
+            menu.setAttribute("hidden", "");
+            overlay.setAttribute("hidden", "");
+            button.removeAttribute("hidden");
+        }
+
+        button.addEventListener("click", showMenu);
+        close.addEventListener("click", hideMenu);
+        overlay.addEventListener("click", hideMenu);
+
+        // Возвращаем функцию очистки для удаления обработчиков событий при размонтировании компонента
+        return () => {
+            button.removeEventListener("click", showMenu);
+            close.removeEventListener("click", hideMenu);
+            overlay.removeEventListener("click", hideMenu);
+        };
+    }, []); // Пустой массив зависимостей означает, что useEffect будет выполняться только после монтирования и размонтирования компонента
+
+    return (
+        <div className="hero__wrapper">
+            <h1 className="hero__header">Mobile Menu Concept</h1>
+            <div className="hero__phone">
+                <button className="menu__button">
+                    <div>
+                        <div></div>
+                    </div>
+                    Quick Menu
+                </button>
+                <section className="menu__body" hidden="hidden">
+                    <div className="menu__header">
+                        <label>
+                            <div></div>
+                        </label>
+                        <p>Quick Navigation</p>
+                        <button title="Close">
+                            <div></div>
+                        </button>
+                    </div>
+                    <div className="menu__links">
+                        <a href="#0">About Us</a>
+                        <a href="#0">Services</a>
+                        <a href="#0">Pricing</a>
+                        <a href="#0">Blog</a>
+                        <a href="#0">Contact</a>
+                    </div>
+                    <div className="menu__contact">
+                        <a href="#0">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="#000000"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                width="24"
+                            >
+                                <path d="M0 0h24v24H0z" fill="none"></path>
+                                <path
+                                    d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"
+                                ></path>
+                            </svg>
+                            <span>Call</span>
+                        </a>
+                        <a href="#0">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="#000000"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                width="24"
+                            >
+                                <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path>
+                                <path d="M0 0h24v24H0z" fill="none"></path>
+                            </svg>
+                            <span>Email</span>
+                        </a>
+                        <a href="#0">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="#000000"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                width="24"
+                            >
+                                <path d="M21.71 11.29l-9-9c-.39-.39-1.02-.39-1.41 0l-9 9c-.39.39-.39 1.02 0 1.41l9 9c.39.39 1.02.39 1.41 0l9-9c.39-.38.39-1.01 0-1.41zM14 14.5V12h-4v3H8v-4c0-.55.45-1 1-1h5V7.5l3.5 3.5-3.5 3.5z"></path>
+                                <path d="M0 0h24v24H0z" fill="none"></path>
+                            </svg>
+                            <span>Directions</span>
+                        </a>
+                    </div>
+                </section>
+                <div className="menu__overlay" hidden="hidden"></div>
+            </div>
+        </div>
+    );
 }
 
 export default AboutUs;
