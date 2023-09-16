fb08b5d350320a003eae723dba81400f40db2e49
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-16 11:53:39 +1000

    Add files via upload

--- src/Components/BottomNavigation/Menu.js
+++ src/Components/BottomNavigation/Menu.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 function Menu() {
-    return <div>Menu</div>;
+    return (
+        <div>
+            <div id="menu">
+                <div className="menu-item" onClick={() => showCards(null)}>
+                    Все
+                </div>
+                <div className="menu-item" onClick={() => showCards("hookah")}>
+                    Кальян
+                </div>
+                <div className="menu-item" onClick={() => showCards("beer")}>
+                    Пиво
+                </div>
+                <div className="menu-item" onClick={() => showCards("shot")}>
+                    Шоты
+                </div>
+                <div className="menu-item" onClick={() => showCards("drink")}>
+                    Напитки
+                </div>
+                <div className="menu-item" onClick={() => showCards("eat")}>
+                    Закуски
+                </div>
+                <div className="menu-item" onClick={() => showCards("kokteil")}>
+                    Коктейли
+                </div>
+            </div>
+            <div className="cards__container">
+                {filteredFoods.map((food) => (
+                    <Card
+                        key={food.id}
+                        food={food}
+                        onAdd={onAdd}
+                        onRemove={onRemove}
+                        cartItems={cartItems}
+                        setCartItems={setCartItems}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Menu;
