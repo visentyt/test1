0887ad3b0cee494dd09dbddcf50762682d5d95a5
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-17 12:13:55 +1000

    Add files via upload

--- src/Components/BottomNavigation/Complaints.js
+++ src/Components/BottomNavigation/Complaints.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Complaints() {
-    return <div>ЛК</div>;
+    const [userName, setUserName] = useState("Загрузка...");
+
+    useEffect(() => {
+        if (window.Telegram && window.Telegram.WebApp) {
+            // Получите имя пользователя из Telegram и установите его
+            const name = window.Telegram.WebApp.getUser().first_name; // предполагаемая функция, вам следует проверить документацию SDK для правильной функции.
+            setUserName(name);
+        }
+    }, []);
+
+    return <div>ЛК: {userName}</div>;
 }
 
 export default Complaints;
