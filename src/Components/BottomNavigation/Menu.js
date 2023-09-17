6ef4022588f81b5e21d9e3615805d25bf52d62f4
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-18 09:58:12 +1000

    Add files via upload

--- src/Components/BottomNavigation/Menu.js
+++ src/Components/BottomNavigation/Menu.js
@@ -87,30 +87,29 @@ function Menu() {
                 <Cart cartItems={cartItems} onRemove={onRemove} />
             ) : (
                 <>
-
-            <div id="menu">
-                <div className="menu-item" onClick={() => showCards(null)}>
-                    Все
-                </div>
-                <div className="menu-item" onClick={() => showCards("hookah")}>
-                    Кальян
-                </div>
-                <div className="menu-item" onClick={() => showCards("beer")}>
-                    Пиво
-                </div>
-                <div className="menu-item" onClick={() => showCards("shot")}>
-                    Шоты
-                </div>
-                <div className="menu-item" onClick={() => showCards("drink")}>
-                    Напитки
-                </div>
-                <div className="menu-item" onClick={() => showCards("eat")}>
-                    Закуски
-                </div>
-                <div className="menu-item" onClick={() => showCards("kokteil")}>
-                    Коктейли
-                </div>
-            </div>
+                    <div id="menu">
+                        <div className="menu-item" onClick={() => showCards(null)}>
+                            Все
+                        </div>
+                        <div className="menu-item" onClick={() => showCards("hookah")}>
+                            Кальян
+                        </div>
+                        <div className="menu-item" onClick={() => showCards("beer")}>
+                            Пиво
+                        </div>
+                        <div className="menu-item" onClick={() => showCards("shot")}>
+                            Шоты
+                        </div>
+                        <div className="menu-item" onClick={() => showCards("drink")}>
+                            Напитки
+                        </div>
+                        <div className="menu-item" onClick={() => showCards("eat")}>
+                            Закуски
+                        </div>
+                        <div className="menu-item" onClick={() => showCards("kokteil")}>
+                            Коктейли
+                        </div>
+                    </div>
                     <div className="cards__container">
                         {filteredFoods.map((food) => (
                             <Card
