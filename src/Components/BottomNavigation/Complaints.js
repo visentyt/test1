541fa6a638675236640967f53e8a25a0fb0b8f40
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-17 15:18:56 +1000

    Add files via upload

--- src/Components/BottomNavigation/Complaints.js
+++ src/Components/BottomNavigation/Complaints.js
@@ -3,14 +3,9 @@ import React from "react";
 function Complaints() {
     const username = window.username;
     const firstname = window.firstname;
-    const photo = window.photo;
-    console.log(username);
-    console.log(firstname);
-    console.log(photo);
 
     return (
         <div>
-            <img src={photo} alt="User" />
             <div>Username: {username}</div>
             <div>Firstname: {firstname}</div>
         </div>
