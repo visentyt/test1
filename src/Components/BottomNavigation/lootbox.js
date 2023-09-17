3970c9842e9e9fa35ca0184496b0a352c92241ba
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-17 15:22:19 +1000

    Add files via upload

---
+++ src/Components/BottomNavigation/lootbox.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./lootbox.css";
+
+function Lootbox() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [result, setResult] = useState("");
+
+    const items = [
+        { name: "Item 1", image: "item1.png", chance: 0.2 },
+        // Добавьте другие предметы здесь
+    ];
+
+    const openLootbox = () => {
+        const random = Math.random();
+        let totalChance = 0;
+
+        for (const item of items) {
+            totalChance += item.chance;
+            if (random <= totalChance) {
+                setResult(`Поздравляем! Вы выиграли: ${item.name}`);
+                setIsOpen(true);
+                break;
+            }
+        }
+    };
+
+    const resetLootbox = () => {
+        setIsOpen(false);
+        setResult("");
+    };
+
+    return (
+        <div>
+            <div>Promotions</div>
+            {isOpen ? (
+                <div>
+                    <div>Результат: {result}</div>
+                    <button onClick={resetLootbox}>Забрать</button>
+                </div>
+            ) : (
+                <div>
+                    <button onClick={openLootbox}>Открыть лутбокс</button>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Lootbox;
