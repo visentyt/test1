3fe43880ed1381c55b94432d24f905669b76ed89
visentyt <68974264+visentyt@users.noreply.github.com>
2023-09-18 14:13:24 +1000

    Add files via upload

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Card.css";
 import Button from "../Button/Button";
 
-function Card({ food, onAdd, onRemove, cartItems, setCartItems }) {
+function Card({ food, onAdd, onRemove, cartItems, setCartItems, handleSendInvoice }) {
     const [count, setCount] = useState(0);
     const { title, Image, price } = food;
 
